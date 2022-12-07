8f42ace233ba49a895e4e6970c5ee86bf7403a30
Aleksandr Guido <iggelds@gmail,com>
2022-12-07 18:16:49 +0100

    adding Spring Data JPA

--- src/main/java/org/example/models/Person.java
+++ src/main/java/org/example/models/Person.java
@@ -20,13 +20,19 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @NotEmpty(message = "Email should not be empty")
+    @Column(name = "email")
+    @Email
+    private String email;
+
     public Person() {
 
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, String email) {
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -51,5 +57,23 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
